86f42cdd0a6678447a9a471d4cee5d336c1b99b2
Uri Khaimov <urikhaimov@gmail.com>
2025-06-21 06:39:01 +0300

    fixed Integration REST API

--- apps/backend/src/firebase/firebase-admin.ts
+++ apps/backend/src/firebase/firebase-admin.ts
@@ -1,10 +1,11 @@
 // src/firebase/firebase-admin.ts
 import * as admin from 'firebase-admin';
-import * as serviceAccount from './service-account.json'; // ✅ ensure this path is correct
+import serviceAccount from './service-account.json'; // ✅ ensure path is correct
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
 
+export { admin }; // ✅ export full SDK
 export const adminAuth = admin.auth();
 export const adminDb = admin.firestore();
