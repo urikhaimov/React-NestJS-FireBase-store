efe26ab145b2995381cbd6b777bd88c1649304ed
Uri Khaimov <urikhaimov@gmail.com>
2025-06-22 18:56:09 +0300

    test real Firestore data next instead of the mocked array.

--- apps/api/src/categories/categories.controller.ts
+++ apps/api/src/categories/categories.controller.ts
@@ -2,12 +2,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 
-@Controller('categories')
+@Controller('categories') // With global prefix, becomes /api/categories
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
   @Get()
   async findAll() {
-     return [{ id: '1', name: 'Shoes' }, { id: '2', name: 'Hats' }];
+    return this.categoriesService.findAll(); // ✅ real Firestore call
   }
 }
