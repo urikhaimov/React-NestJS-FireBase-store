b038fe2b3c1c011ad849b19f75a9a5ce86405739
Uri Khaimov <urikhaimov@gmail.com>
2025-06-22 18:56:09 +0300

    test real Firestore data next instead of the mocked array.

--- apps/backend/src/categories/categories.service.ts
+++ apps/backend/src/categories/categories.service.ts
@@ -1,15 +1,14 @@
 // src/categories/categories.service.ts
 import { Injectable } from '@nestjs/common';
 import { adminDb } from '../firebase/firebase-admin';
-import { collection, getDocs } from 'firebase/firestore';
 
 @Injectable()
 export class CategoriesService {
-  async getAllCategories() {
- const snapshot = await adminDb.collection('categories').get();
-const categories = snapshot.docs.map(doc => ({
-  id: doc.id,
-  ...doc.data(),
-}));
+  async findAll() {
+    const snapshot = await adminDb.collection('categories').get();
+    return snapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
   }
 }
