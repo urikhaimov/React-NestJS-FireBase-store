b01011fedd723935b1e3e624043c4551372fe52f
Uri Khaimov <urikhaimov@gmail.com>
2025-06-25 20:35:33 +0300

    config to upload pictures

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -3,14 +3,15 @@ import react from '@vitejs/plugin-react';
 
 const csp = [
   "default-src 'self'",
-  "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com",
+  "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://apis.google.com",
   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
   "font-src 'self' https://fonts.gstatic.com https://js.stripe.com",
-  "connect-src 'self' http://localhost:3000 https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firestore.googleapis.com https://firebase.googleapis.com https://firebasestorage.googleapis.com https://storage.googleapis.com",
+  "connect-src 'self' http://localhost:3000 https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firestore.googleapis.com https://firebase.googleapis.com https://www.googleapis.com https://firebasestorage.googleapis.com https://apis.google.com",
+  "img-src 'self' data: blob: https://firebasestorage.googleapis.com",
+  "media-src 'self' data: blob: https://firebasestorage.googleapis.com",
   "frame-src https://js.stripe.com https://hooks.stripe.com",
-  "worker-src 'self' blob: https://js.stripe.com",
-  "img-src 'self' data: https://firebasestorage.googleapis.com"
-].join('; '); // ✅ No newline characters
+  "worker-src 'self' blob: https://js.stripe.com"
+].join('; ');
 
 export default defineConfig({
   plugins: [react()],
