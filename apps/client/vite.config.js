bdec7c4633ae45bfe1bfabbfcf25a76c4e5393d3
Uri Khaimov <urikhaimov@gmail.com>
2025-06-27 17:49:19 +0300

    start checkout process

--- apps/client/vite.config.js
+++ apps/client/vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 const csp = [
     "default-src 'self'",
-    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://apis.google.com",
+    "script-src 'self' 'unsafe-inline' https://js.stripe.com https://www.googletagmanager.com",
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-    "font-src 'self' https://fonts.gstatic.com https://js.stripe.com",
-    "img-src 'self' data: blob: https://firebasestorage.googleapis.com https://firebasestorage-download.googleapis.com https://placehold.co https://picsum.photos",
-    "connect-src 'self' http://localhost:3000 https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firestore.googleapis.com https://firebase.googleapis.com https://www.googleapis.com https://apis.google.com https://firebasestorage.googleapis.com https://firebasestorage-download.googleapis.com",
-    "frame-src https://js.stripe.com https://hooks.stripe.com https://onlinestoretemplate-59d3e.firebaseapp.com https://accounts.google.com https://apis.google.com",
+    "font-src 'self' https://fonts.gstatic.com https://js.stripe.com https://cdn.jsdelivr.net",
+    "img-src 'self' data: blob: https://firebasestorage.googleapis.com https://firebasestorage-download.googleapis.com",
+    "connect-src 'self' http://localhost:3000 https://api.stripe.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firestore.googleapis.com https://firebase.googleapis.com https://www.googleapis.com https://apis.google.com",
+    "frame-src https://js.stripe.com https://hooks.stripe.com https://onlinestoretemplate-59d3e.firebaseapp.com",
     "worker-src 'self' blob:"
 ].join('; ');
 export default defineConfig({
