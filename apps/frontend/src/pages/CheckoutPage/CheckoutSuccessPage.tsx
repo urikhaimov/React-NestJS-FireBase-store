cd52f75dffda764e08cffcc2d71f0c41c77f7c83
Uri Khaimov <urikhaimov@gmail.com>
2025-07-04 09:22:26 +0300

    orderDetailsPage

--- apps/frontend/src/pages/CheckoutPage/CheckoutSuccessPage.tsx
+++ apps/frontend/src/pages/CheckoutPage/CheckoutSuccessPage.tsx
@@ -1,9 +1,18 @@
-// src/pages/CheckoutSuccessPage.tsx
 import { Typography, Box } from '@mui/material';
 
 export default function CheckoutSuccessPage() {
   return (
-    <Box sx={{ mt: 6, textAlign: 'center' }}>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: 'calc(100vh - 64px)', // Adjust if you have a persistent header
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        px: 2,
+      }}
+    >
       <Typography variant="h4" gutterBottom>
         🎉 Payment Successful!
       </Typography>
