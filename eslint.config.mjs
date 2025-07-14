26f4b0260ade10eadac32706e39bebdcc9479bc7
Volodymyr Tkach <teamco@gmail.com>
2025-07-14 10:35:18 +0300

    feat: enhance ESLint configuration with additional plugins and rules for improved code quality

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,20 @@
 import nx from '@nx/eslint-plugin';
+import jsLint from '@eslint/js';
+import tsLint from '@typescript-eslint/eslint-plugin';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+// @ts-ignore
+import json from 'eslint-plugin-json';
+// @ts-ignore
+import * as importPlugin from 'eslint-plugin-import';
+import jsdoc from 'eslint-plugin-jsdoc';
+import * as mdx from 'eslint-plugin-mdx';
 
 export default [
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
+  jsLint.configs.recommended,
   {
     ignores: [
       '**/dist',
@@ -41,6 +52,83 @@ export default [
       '**/*.mjs',
     ],
     // Override or add rules here
-    rules: {},
+    plugins: {
+      jsdoc,
+      import: importPlugin,
+      json,
+      mdx,
+      '@typescript-eslint': tsLint,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tsLint.configs.recommended.rules,
+      ...prettierConfig.rules,
+      '@typescript-eslint/no-unused-vars': 'warn',
+      // 'no-console': 'warn',
+      'no-debugger': 'warn',
+      'no-duplicate-imports': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      // Enforce consistent indentation (4 spaces in this case)
+      // indent: ['warn', 2],
+      // Enforce the use of single quotes for strings
+      quotes: ['warn', 'single'],
+      // Enforce semicolons at the end of statements
+      semi: ['error', 'always'],
+      // Enforce consistent line breaks (LF for Unix)
+      'linebreak-style': ['error', 'unix'],
+      // Require the use of === and !== (no implicit type conversions)
+      eqeqeq: ['error', 'always'],
+      // Enforce a maximum line length (usually 80 or 100 characters)
+      'max-len': [
+        'warn',
+        {
+          code: 80,
+          ignoreComments: true,
+          ignoreTrailingComments: true,
+          ignoreUrls: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+          ignoreRegExpLiterals: true,
+          ignorePattern: '^\\s*import\\s+.*\\s+from\\s+["\'].*["\']',
+        },
+      ],
+      // Enable Prettier as a lint rule
+      'prettier/prettier': [
+        'error',
+        {
+          trailingComma: 'all',
+          semi: true,
+          singleQuote: true,
+        },
+      ],
+      'import/no-unresolved': 0,
+      'import/extensions': 1,
+      'import/named': 1,
+      'import/namespace': 1,
+      'import/default': 1,
+      'import/export': 1,
+      'jsdoc/require-param': ['warn', { contexts: ['TSParameterProperty'] }],
+      'jsdoc/require-param-description': [
+        'warn',
+        { contexts: ['TSParameterProperty'] },
+      ],
+      'jsdoc/require-param-name': [
+        'warn',
+        { contexts: ['TSParameterProperty'] },
+      ],
+      'jsdoc/require-returns': ['warn', { contexts: ['TSPropertySignature'] }],
+      'jsdoc/require-returns-description': [
+        'warn',
+        { contexts: ['TSPropertySignature'] },
+      ],
+      'jsdoc/require-returns-type': [
+        'warn',
+        { contexts: ['TSPropertySignature'] },
+      ],
+      'jsdoc/require-description': [
+        'warn',
+        { contexts: ['TSPropertySignature'] },
+      ],
+    },
   },
 ];
